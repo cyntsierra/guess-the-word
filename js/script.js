d0464dba24d9fc2afe07279287b97d4a896f9fd6
cynthia_s <sierracynt@gmail.com>
2021-08-11 14:52:31 +0100

    Updated Guess Game

--- js/script.js
+++ js/script.js
@@ -1,30 +1,53 @@
-const guessedLetterElement = document.querySelector(".guessed-letters");
-const guessButton = document.querySelector(".guess");
-const guessLetterInput=document.querySelector(".letter");
+const guessedLettersElement = document.querySelector(".guessed-letters");
+const guessLetterButton = document.querySelector(".guess");
+const letterInput = document.querySelector(".letter");
 const wordInProgress = document.querySelector(".word-in-progress");
-const remainingGuesses=document.querySelector(".remaining");
-const remainingSpan=document.querySelector(".remaining span");
-const message=document.querySelector(".message");
-const playAgainButton=document.querySelector(".play-again");
+const remainingGuessesElement = document.querySelector(".remaining");
+const remainingGuessesSpan = document.querySelector(".remaining span");
+const message = document.querySelector(".message");
+const playAgainButton = document.querySelector(".play-again");
 
 const word = "magnolia";
+const guessedLetters=[];
 
-const placeholder = function(word){
-    //use an array:
-    const placeholderLetters =[];
+const placeholder = function (word){
+    const placeholderLetters=[];
     for (const letter of word){
-        console.log(letter);
+        //console.log(letter);
         placeholderLetters.push("●");
     }
-    wordInProgress.innerText=placeholderLetters.join("");
+   wordInProgress.innerText=placeholderLetters.join("");
 
 };
-placeholder(word);
+placeholder (word);
 
-guessButton.addEventListener("click",function(e){
+guessLetterButton.addEventListener("click",function(e){
     e.preventDefault();
-    const inputValue=guessLetterInput.value;
-     console.log(inputValue);
-     guessLetterInput.value="";
+    message.innerText="";
+    const guess = letterInput.value;
+    const goodGuess=validateInput(guess);
+    if(goodGuess){makeGuess(guess);
+    }
+    letterInput.value="";
+});
+
+const validateInput = function (input){
+    const acceptedLetter=/[a-zA-Z]/;
+    if (input.length===0){
+        message.innerText="Please type in a letter";
+    } else if (input.length>1){
+        message.innerText="Only one at a time please!";
+    }
+    else if (!input.match(acceptedLetter)){
+        message.innerText="Please enter a letter from A-Z";
+    } else {return input;}
+};
+
+const makeGuess=function(guess){
+    guess=guess.toUpperCase();
+    if (guessedLetters.includes(guess)){
+        message.innerText="You already guessed that, silly, try again";
+    }else {guessedLetters.push(guess);
+    console.log(guessedLetters);
 }
-);
+    };
